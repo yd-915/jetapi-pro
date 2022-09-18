a41666eb142a130f35e858e17d561c134d7b047e
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-09-18 02:40:27 +0000

    Update second.js

--- data/second.js
+++ data/second.js
@@ -4,16 +4,20 @@ const second =
     "Plane": "English Electric Lightning",
     "results": [
       {
-        "definition": "Supersonic jet fighter aircraft of the Cold War era",
+        "Definition": "Supersonic jet fighter aircraft of the Cold War era",
+        "Introduction": "1960",
+        "National Origin": "U.K.",
         "First flight": "4 April 1957",
         "Retired": "1988",
-        "weight": "14,090 kg",
-        "wingspan": "11m",
+        "Weight": "14,090 kg",
+        "Lenght": "11 m",
+        "Wingspan": "17m",
+        "Units": "337",
         "Range": "1,287 km",
         "Top Speed": "2,414 km/h",
         "Engine type": "Rolls-Royce Avon",
         "Manufacturer(s)": "English Electric, British Aircraft Corporation",
-        "image":"https://imgs.search.brave.com/WSowhv4WZNZqGdxfbd64D-6KaSALptZub8VVb2k-_0s/rs:fit:717:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5V/Q2xHT2FRWlU1SW9v/SVZIbFNnYnlBSGFF/NSZwaWQ9QXBp",
+        "Image":"https://imgs.search.brave.com/WSowhv4WZNZqGdxfbd64D-6KaSALptZub8VVb2k-_0s/rs:fit:717:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5V/Q2xHT2FRWlU1SW9v/SVZIbFNnYnlBSGFF/NSZwaWQ9QXBp",
         "Countries used": [
           "Kuwait",
           "Saudi Arabia",
@@ -30,16 +34,20 @@ const second =
     "Plane": "Dassault Mirage III",
     "results": [
       {
-        "definition": "Supersonic Fighter Aircraft ",
+        "Definition": "Supersonic Fighter Aircraft ",
+        "Introduction": "1961",
+        "National Origin": "France",
         "First flight": "17 November 1956",
         "Retired": "N/A",
-        "weight": "13,700 kg",
-        "wingspan": "8.22 m",
+        "Weight": "13,700 kg",
+        "Lenght": "15.03 m",
+        "Wingspan": "8.22 m",
+        "Units": "1,422",
         "Range": "2,408 km",
-        "Top Speed": "2,716.56 km/h",
+        "Top Speed": "2,716 km/h",
         "Engine type": "Atar 9C-3",
         "Manufacturer(s)": "Dassault Aviation",
-        "image": "https://imgs.search.brave.com/ZfwSsHAzoqgKpgxPWdQXNT7e1yBYt2S8pSZx_iGmLp8/rs:fit:704:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC42/V2MtS2lEdjZIenAy/aV9GcEtjcHl3SGFF/XyZwaWQ9QXBp",
+        "Image": "https://imgs.search.brave.com/ZfwSsHAzoqgKpgxPWdQXNT7e1yBYt2S8pSZx_iGmLp8/rs:fit:704:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC42/V2MtS2lEdjZIenAy/aV9GcEtjcHl3SGFF/XyZwaWQ9QXBp",
         "Used by": [
           "Pakistan",
           
@@ -53,16 +61,20 @@ const second =
     "Plane": "MiG-21",
     "results": [
       {
-        "definition": "A supersonic jet fighter and interceptor aircraft",
+        "Definition": "A supersonic jet fighter and interceptor aircraft",
+        "Introduction": "1959",
+        "National Origin": "Soviet Union",
         "First flight": "February 14, 1955",
         "Retired": "1980s",
-        "weight": "5,200 kg",
-        "wingspan": "7.15 m",
+        "Weight": "5,200 kg",
+        "Lenght": "15.03 m",
+        "Wingspan": "7.15 m",
+        "Units": "11,496",
         "Range": "644 km",
         "Top Speed": "2,229 km/h",
         "Engine type": "Tumansky R-11",
         "Manufacturer(s)": "Mikoyan-Gurevich",
-        "image": "https://imgs.search.brave.com/3XPZTJUM1jnbc86FqjNDXrbSuALswhIszi_ylYas-J4/rs:fit:881:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/LVp4MFVoZGJIYW80/aGpCdTB3b09nSGFE/XyZwaWQ9QXBp",
+        "Image": "https://imgs.search.brave.com/3XPZTJUM1jnbc86FqjNDXrbSuALswhIszi_ylYas-J4/rs:fit:881:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/LVp4MFVoZGJIYW80/aGpCdTB3b09nSGFE/XyZwaWQ9QXBp",
         "Countries used": [
           "Croatia",
           "Azerbaijan",
@@ -92,18 +104,22 @@ const second =
     "Plane": "HAL HF-24",
     "results": [
       {
-        "definition": "Indian fighter-bomber aircraft ",
+        "Definition": "Indian fighter-bomber aircraft ",
+        "Introduction": "1967",
+        "National Origin": "India",
         "First flight": "17 June 1961 ",
         "Retired": "1985",
-        "weight": "10,908 kg",
-        "wingspan": "9 m",
+        "Weight": "10,908 kg",
+        "Lenght":"16 m",
+        "Wingspan": "9 m",
+        "Units": "147",
         "Range": "800 km",
         "Top Speed": "1,112 km/h",
         "Engine type": "Bristol Siddeley Orpheus Mk 703",
         "Manufacturer(s)": "Hindustan Aeronautics",
-        "image": "https://imgs.search.brave.com/syYcsd2NPIfy5dYbPTdWzymEEUPdQ51GDfK9ZKJ56uw/rs:fit:682:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5R/a1BZSjY1VVg5LWxN/SzhxU0NPTS13SGFG/SiZwaWQ9QXBp",
+        "Image": "https://imgs.search.brave.com/syYcsd2NPIfy5dYbPTdWzymEEUPdQ51GDfK9ZKJ56uw/rs:fit:682:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5R/a1BZSjY1VVg5LWxN/SzhxU0NPTS13SGFG/SiZwaWQ9QXBp",
         "Countries used": [
-          "N/A"
+          "India"
       
       ],
         
@@ -115,10 +131,14 @@ const second =
     "results": [
       {
         "definition": "Swedish fighter aircraft",
+        "Introduction": "1960",
+        "National Origin": "Sweden",
         "First flight": "25 October 1955",
         "Retired": "1993",
-        "weight": "7,865 kg",
-        "wingspan": "9.42 m",
+        "Weight": "7,865 kg",
+        "Lenght":"15 m",
+        "Wingspan": "9.42 m",
+        "Units": "651",
         "Range": "3,250 km",
         "Top Speed": "2,124 km/h",
         "Engine type": "Svenska Flygmotor RM6B/C",
@@ -138,11 +158,15 @@ const second =
     "Plane": "Convair F-106 Delta Dart",
     "results": [
       {
-        "definition": "Interceptor aircraft",
+        "Definition": "Interceptor aircraft",
+        "Introduction": "1959",
+        "National Origin": "U.S.A.",
         "First flight": "December 26, 1956",
         "Retired": "1998",
-        "weight": "11,080 kg",
-        "wingspan": "12 m",
+        "Weight": "11,080 kg",
+        "Lenght":"22 m",
+        "Wingspan": "12 m",
+        "Units":"342",
         "Range": "2,897 km",
         "Top Speed": "2,455 km/h",
         "Engine type": "Pratt & Whitney J75",
@@ -159,11 +183,15 @@ const second =
     "Plane": "Lockheed F-104 Starfighter",
     "results": [
       {
-        "definition": "Single-engine, supersonic air superiority fighter",
-        "First flight": "4 March 1954 ",
+        "Definition": "Single-engine, supersonic air superiority fighter",
+        "Introduction": "1958",
+        "National Origin": "U.S.A.",
+        "First flight": "4 March 1954",
         "Retired": "1975",
-        "weight": "6,387 kg",
-        "wingspan": "6.36 m",
+        "Weight": "6,387 kg",
+        "Lenght": "17 m",
+        "Wingspan": "6.36 m",
+        "Units": "2,578",
         "Range": "2,012 km",
         "Top Speed": "2,717 km/h",
         "Engine type": "General Electric J79",
@@ -175,6 +203,83 @@ const second =
           "Denmark",
           "Finland",
           "Sweden",
+       
+      ],
+        
+      },
+    ]
+  },
+  {
+    "Plane": "Hawker Hunter",
+    "results": [
+      {
+        "Definition": "Transonic British jet-powered fighter aircraft",
+        "Introduction": "1954",
+        "National Origin": "U.K.",
+        "First flight": "July 20, 1951",
+        "Retired": "2014",
+        "Weight": "6,405 kg",
+        "Lenght": "14 m",
+        "Wingspan": "10.26 m",
+        "Units": "1,972",
+        "Range": "3,060 km",
+        "Top Speed": "1,150 km/h",
+        "Engine type": "Rolls-Royce Avon",
+        "Manufacturer(s)": "Hawker Aircraft, Hawker Siddeley",
+        "image": "https://imgs.search.brave.com/0H3FWZMn7Iik5aiqi51UZqh3ctghUE-LD9Xf3Szy5SI/rs:fit:960:640:1/g:ce/aHR0cHM6Ly93d3cu/Zmx5aW5nLXRpZ2Vy/cy5jby51ay93cC1j/b250ZW50L3VwbG9h/ZHMvMjAxNi8xMS9I/SDYtSGF3a2VyLUh1/bnRlci1GNi05NjB4/NjQwLmpwZw",
+        "Countries used": [
+          "U.K.",
+          "Austria",
+          "Denmark",
+          "Zimbabwe",
+          "Sweden",
+          "Somalia",
+          "Oman",
+          "Belgium",
+          "Iraq",
+          "Kuwait",
+          "Saudi Arabia",
+          "Signapore",
+          "Kenya",
+          "Qatar",
+          "Jordan",
+          "Netherlands",
+          "India",
+          "Chile",
+          "Peru",
+          "Lebanon",
+       
+      ],
+        
+      },
+    ]
+  },
+   {
+    "Plane": "Fiat G.91",
+    "results": [
+      {
+        "Definition": "Italian jet fighter aircraft",
+        "Introduction": "1958",
+        "National Origin": "Italy",
+        "First flight": "9 August 1956",
+        "Retired": "1995",
+        "Weight": "3,100 kg",
+        "Lenght": "10.3 m",
+        "Wingspan": "8.56m",
+        "Units": "770",
+        "Range": "1,150 km",
+        "Top Speed": "1,075 km/h",
+        "Engine type": "Bristol Siddeley Orpheus",
+        "Manufacturer(s)": "Aeritalia, Fiat Aviazione, Alenia Aeronautica, Flugzeug-Union Süd",
+        "image": "https://imgs.search.brave.com/gpeRfoP6zfeLvyFUDiQUVknlYWOH8uXG3bBcFsAW_as/rs:fit:734:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5U/OEk4Z3J0My1IaU5B/b0JRXy1YTzRBSGFF/eSZwaWQ9QXBp",
+        "Countries used": [
+          "U.S.A.",
+          "Germany",
+          "Italy",
+          "Greece",
+          "Portugal",
+         
+         
        
       ],
         
