1fbd9f6f95d0e439e27038690c9c7707e78004f5
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-09-18 17:04:23 +0000

    Update third.js

--- data/third.js
+++ data/third.js
@@ -4,16 +4,20 @@ const third =
     "Plane": "McDonnell Douglas F-4 Phantom II",
     "results": [
       {
-        "definition": "Twin-engine, all-weather, third generation fighter-bomber",
+        "Definition": "Twin-engine, all-weather, third generation fighter-bomber",
+        "Introduction": "1960",
+        "National Origin": "U.S.A",
         "First flight": "May 27 1958",
         "Retired": "1998",
-        "weight": "25,218 kg",
-        "wingspan": "12 m",
+        "Weight": "25,218 kg",
+        "Lenght": "19 m",
+        "Wingspan": "12 m",
+        "Units": "5,195",
         "Range": "2,816 km",
         "Top Speed": "2,370 km/h",
         "Engine type": "General Electric J79",
         "Manufacturer(s)": "McDonnell Douglas, McDonnell Aircraft Corporation",
-        "image": "https://imgs.search.brave.com/XYOYdAKA8zy9DF88BXKeXrSIKJ54uzIaMWG66m4GW7c/rs:fit:704:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5p/dE9xTE5xX0sxdE5I/dl9HS1JpbWp3SGFF/XyZwaWQ9QXBp",
+        "Image": "https://imgs.search.brave.com/XYOYdAKA8zy9DF88BXKeXrSIKJ54uzIaMWG66m4GW7c/rs:fit:704:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5p/dE9xTE5xX0sxdE5I/dl9HS1JpbWp3SGFF/XyZwaWQ9QXBp",
         "Countries used": [
           "Iran",
           "Greece",
@@ -28,16 +32,20 @@ const third =
     "Plane": "Dassault Mirage F1",
     "results": [
       {
-        "definition": "Military aircraft designed and built by French Dassault as successor of the fighter Mirage III.",
+        "Definition": "Military aircraft designed and built by French Dassault as successor of the fighter Mirage III.",
+        "Introduction": "1973",
+        "National Origin": "France",
         "First flight": "December 23, 1966",
         "Retired": "2014 (in France)",
-        "weight": "7,400 kg",
-        "wingspan": "8.4 m",
+        "Weight": "7,400 kg",
+        "Lenght": "19 m",
+        "Wingspan": "8.4 m",
+        "Units": "720",
         "Range": "3,300 km",
         "Top Speed": "2,338 km/h",
         "Engine type": "Snecma Atar",
         "Manufacturer(s)": "Dassault Aviation",
-        "image": "",
+        "Image": "https://imgs.search.brave.com/l0LVgNIMAiLtgTg-xJWLieHjXoCaB0vG8G_JO8NM2kg/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC53/UFdHd1gyY0Rhc21O/VExUbXkySXRRSGFF/NyZwaWQ9QXBp",
         "Countries used": [
           "Republic of the Congo",
           "Libya",
@@ -55,18 +63,22 @@ const third =
     "Plane": "Mitsubishi F-1",
     "results": [
       {
-        "definition": "Swept-wing, single-seat, twin-engine supersonic strike aircraft",
+        "Definition": "Swept-wing, single-seat, twin-engine supersonic strike aircraft",
+        "Introduction": "1978",
+        "National Origin": "Japan",
         "First flight": "December 23, 1966",
         "Retired": "March 2006",
-        "weight": "6,358 kg",
-        "wingspan": "7.6 m",
+        "Weight": "6,358 kg",
+        "Lenght": "18 m",
+        "Wingspan": "7.6 m",
+        "Units": "77",
         "Range": "3,300 km",
         "Top Speed": "1,700 km/h",
         "Engine type": "Rolls-Royce Turbomeca Adour",
         "Manufacturer(s)": "Mitsubishi Heavy Industries, Ltd., Mitsubishi Aircraft Company",
         "image": "https://imgs.search.brave.com/Y5Ax0nQDpPnv7dIrd4V1qpz26n57BNBSiqsMgfWk-5s/rs:fit:766:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5v/cWlZcU1PZUNuR1l6/RGdvbk01X0N3SGFF/bCZwaWQ9QXBp",
         "Countries used": [
-        "N/A"
+        "Japan"
       ],
         
       },
@@ -76,16 +88,20 @@ const third =
     "Plane": "Shenyang J-8",
     "results": [
       {
-        "definition": "High-speed, high-altitude Chinese-built single-seat interceptor fighter aircraft",
+        "Definition": "High-speed, high-altitude Chinese-built single-seat interceptor fighter aircraft",
+        "Introduction": "1980",
+        "National Origin": "China",
         "First flight": "July 5, 1969",
         "Retired": "N/A",
-        "weight": "15,288 kg",
-        "wingspan": "9.3 m",
+        "Weight": "15,288 kg",
+        "Length": "21.6 m",
+        "Wingspan": "9.3 m",
+        "Units": ">408",
         "Range": "3,300 km",
         "Top Speed": "2,339 km/h",
         "Engine type": "Guizhou WP-13B",
         "Manufacturer(s)": "Shenyang Aircraft Corporation",
-        "image": "https://imgs.search.brave.com/jKLhkEGH1A8kWjTd8d-YNVzDcadH08-LBlKjl9bV1AQ/rs:fit:702:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/YzdFTlo5ZkF1WEJq/LUx0MnpSSXZBSGFG/QSZwaWQ9QXBp",
+        "Image": "https://imgs.search.brave.com/jKLhkEGH1A8kWjTd8d-YNVzDcadH08-LBlKjl9bV1AQ/rs:fit:702:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/YzdFTlo5ZkF1WEJq/LUx0MnpSSXZBSGFG/QSZwaWQ9QXBp",
         "Countries used": [
         "China"
       ],
@@ -97,18 +113,23 @@ const third =
     "Plane": "Sukhoi Su-15",
     "results": [
       {
-        "definition": " A twin-engined supersonic interceptor",
+        "Definition": "A twin-engined supersonic interceptor",
+        "Introduction": "1965",
+        "National Origin": "Soviet Union",
         "First flight": "30 May 1962 ",
         "Retired": "1996 (Ukraine)",
-        "weight": "10,760 kg",
-        "wingspan": "9.4 m",
+        "Weight": "10,760 kg",
+        "Length": "21.6 m",
+        "Wingspan": "9.4 m",
+        "Units": "1,290",
         "Range": "1,050 km",
-        "Top Speed": " 3,063 km/h",
-        "Engine type": "Tumansky R-11",
+        "Top Speed": "3,063 km/h",
+        "Engine type": "Tumansky R-13F-300",
         "Manufacturer(s)": "Sukhoi",
-        "image": "https://imgs.search.brave.com/mJXU-jrK8SKv2E_gS23J2luvOWMOM1p0HoFojMakwTw/rs:fit:724:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4t/VWlFRHYyRUhrMW1N/QWNwN0N1RXZBSGFF/MiZwaWQ9QXBp",
+        "Image": "https://imgs.search.brave.com/mJXU-jrK8SKv2E_gS23J2luvOWMOM1p0HoFojMakwTw/rs:fit:724:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4t/VWlFRHYyRUhrMW1N/QWNwN0N1RXZBSGFF/MiZwaWQ9QXBp",
         "Countries used": [
-        "China"
+        "Soviet Union/Russia",
+        "Ukraine"
       ],
         
       },
@@ -118,19 +139,22 @@ const third =
     "Plane": "Convair F-106 Delta Dart",
     "results": [
       {
-        "definition": "Primary all-weather interceptor aircraft",
+        "Definition": "Primary all-weather interceptor aircraft",
+        "Introduction": "1959",
+        "National Origin": "U.S.A.",
         "First flight": "26 December 1956",
         "Retired": "August 1986",
-        "weight": "23,760 lbs",
-        "wingspan": "38 ft",
-        "Range": "1,500 mi",
-        "Top Speed": "1,525 mph",
+        "Weight": "23,760 lbs",
+        "Length": "22 m"
+        "Wingspan": "12 m",
+         "Units": "1,290",
+        "Range": "2,897 km",
+        "Top Speed": "2,455 km/h",
         "Engine type": "Pratt & Whitney J75-P-17",
         "Manufacturer(s)": "Convair",
-        "image": "https://amcmuseum.org/wp-content/uploads/2014/10/F-106-59-0023-177-FIG-NJ-ANG-1985.png",
+        "Image": "https://amcmuseum.org/wp-content/uploads/2014/10/F-106-59-0023-177-FIG-NJ-ANG-1985.png",
         "Countries used": [
-       
-        "United States"
+         "United States"
       ],
         
       },
