8176f09ec6a2e8f400b7253fcc89eda560eae60a
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-09-20 16:17:21 +0000

    Update checkout.js

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,1 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var checkout = require('../views/checkout');
 
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(checkout);
+});
+
+module.exports = router
