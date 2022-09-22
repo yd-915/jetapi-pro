f1c489b0d2b9f89493d2a2692a80e7b77ea9f9c9
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-09-22 21:55:15 +0000

    Update fifth.js

--- data/fifth.js
+++ data/fifth.js
@@ -4,16 +4,16 @@ const five =
     "Plane": "Lockheed Martin F-22 Raptor",
     "results": [
       {
-        "definition": "Stealth fighter aircraft",
+        "Definition": "Stealth fighter aircraft",
         "First flight": "September 7, 1997",
         "Retired": "N/A",
-        "weight": "19,700 kg",
-        "wingspan": "14 m",
+        "Weight": "19,700 kg",
+        "Wingspan": "14 m",
         "Range": "3,000 km",
         "Top Speed": "2,414 km/h",
         "Engine type": "Pratt & Whitney F119-PW-100",
         "Manufacturer(s)": "Lockheed Martin",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/F-22_Raptor_edit1_%28cropped%29.jpg/300px-F-22_Raptor_edit1_%28cropped%29.jpg",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/F-22_Raptor_edit1_%28cropped%29.jpg/300px-F-22_Raptor_edit1_%28cropped%29.jpg",
         "Countries used": [
           "U.S.A.",
          
@@ -26,16 +26,16 @@ const five =
     "Plane": "Lockheed Martin F-35A Lightning II",
     "results": [
       {
-        "definition": "conventional takeoff and landing (CTOL) variant of the F-35 intended for the USAF and other air forces",
+        "Definition": "conventional takeoff and landing (CTOL) variant of the F-35 intended for the USAF and other air forces",
         "First flight": "15 December 2006",
         "Retired": "N/A",
-        "weight": "13,290 kg",
-        "wingspan": "10.7 m",
+        "Weight": "13,290 kg",
+        "Wingspan": "10.7 m",
         "Range": "2,200 km",
         "Top Speed": "1,960 km/h ",
         "Engine type": "Pratt & Whitney F135-PW-100",
         "Manufacturer(s)": "Lockheed Martin",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/F-35A_flight_%28cropped%29.jpg/300px-F-35A_flight_%28cropped%29.jpg",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/F-35A_flight_%28cropped%29.jpg/300px-F-35A_flight_%28cropped%29.jpg",
         "Used by": [
           "Australia",
           "Finland",
@@ -59,16 +59,16 @@ const five =
     "Plane": "Lockheed Martin F-35B Lightning II",
     "results": [
       {
-        "definition": "Short takeoff and vertical landing (STOVL) variant of the aircraft",
+        "Definition": "Short takeoff and vertical landing (STOVL) variant of the aircraft",
         "First flight": "June 11, 2008",
         "Retired": "N/A",
-        "weight": "14,700 kg",
-        "wingspan": "11 m",
+        "Weight": "14,700 kg",
+        "Wingspan": "11 m",
         "Range": "2,200 km",
         "Top Speed": "1,976 km/h",
         "Engine type": "Pratt & Whitney F135-PW-600",
         "Manufacturer(s)": "Lockheed Martin",
-        "image": "https://images02.military.com/sites/default/files/media/equipment/military-aircraft/f-35b-lightning-ii/2014/02/f-35b-lightning-ii_004.jpg",
+        "Image": "https://images02.military.com/sites/default/files/media/equipment/military-aircraft/f-35b-lightning-ii/2014/02/f-35b-lightning-ii_004.jpg",
         "Used by": [
           "Japan",
           "Italy",
@@ -87,16 +87,16 @@ const five =
     "Plane": "Lockheed Martin F-35C Lightning II",
     "results": [
       {
-        "definition": "The U.S. Navy's first low-observable carrier-based aviation platform.",
+        "Definition": "The U.S. Navy's first low-observable carrier-based aviation platform.",
         "First flight": "June 6, 2010",
         "Retired": "N/A",
-        "weight": "15,800 kg",
-        "wingspan": "10.7 m",
+        "Weight": "15,800 kg",
+        "Wingspan": "10.7 m",
         "Range": "2,200 km",
         "Top Speed": "1,931 km/h",
         "Engine type": "Pratt & Whitney F135-PW-600",
         "Manufacturer(s)": "Lockheed Martin",
-        "image":"https://images05.military.com/sites/default/files/media/equipment/military-aircraft/f-35c-lightning-ii/2014/02/f-35c-lightning-ii_004.jpg",
+        "Image":"https://images05.military.com/sites/default/files/media/equipment/military-aircraft/f-35c-lightning-ii/2014/02/f-35c-lightning-ii_004.jpg",
         "Used by": [
           "United States",
           
@@ -111,16 +111,16 @@ const five =
     "Plane": "Sukhoi Su-57",
     "results": [
       {
-        "definition": "Interceptor aircraft",
+        "Definition": "Interceptor aircraft",
         "First flight": "January 29, 2010",
         "Retired": "N/A",
-        "weight": "18,500 kg",
+        "Weight": "18,500 kg",
         "wingspan": "14 m",
         "Range": "5,500 km",
         "Top Speed": " 2,130 km/h",
         "Engine type": "Lyulka-Saturn AL-41F1",
         "Manufacturer(s)": "United Aircraft Corporation",
-        "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Sukhoi_Design_Bureau%2C_054%2C_Sukhoi_Su-57_%2849581303977%29.jpg/300px-Sukhoi_Design_Bureau%2C_054%2C_Sukhoi_Su-57_%2849581303977%29.jpg",
+        "Image":"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Sukhoi_Design_Bureau%2C_054%2C_Sukhoi_Su-57_%2849581303977%29.jpg/300px-Sukhoi_Design_Bureau%2C_054%2C_Sukhoi_Su-57_%2849581303977%29.jpg",
         "Countries used": [
           "Russia"
       ],
@@ -132,16 +132,16 @@ const five =
     "Plane": "Chengdu J-20",
     "results": [
       {
-        "definition": "Fifth-generation, stealth, twin-engine fighter aircraft",
+        "Definition": "Fifth-generation, stealth, twin-engine fighter aircraft",
         "First flight": "11 January 2011",
         "Retired": "N/A",
-        "weight": "17,000 kg",
-        "wingspan": "14 m",
+        "Weight": "17,000 kg",
+        "Wingspan": "14 m",
         "Range": "4,500 km",
         "Top Speed": "2,126 km/h",
         "Engine type": "Xian WS-15",
         "Manufacturer(s)": "Chengdu Aircraft Industry Group",
-        "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/J-20_at_CCAS2022_%2820220827103424%29.jpg/300px-J-20_at_CCAS2022_%2820220827103424%29.jpg",
+        "Image":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/J-20_at_CCAS2022_%2820220827103424%29.jpg/300px-J-20_at_CCAS2022_%2820220827103424%29.jpg",
         "Countries used": [
           "China",
       ],
@@ -153,16 +153,16 @@ const five =
     "Plane": "Eurofighter Typhoon",
     "results": [
       {
-        "definition": "Twin-engine, canard–delta wing, multirole fighter",
+        "Definition": "Twin-engine, canard–delta wing, multirole fighter",
         "First flight": "27 March 1994",
         "Retired": "N/A",
-        "weight": "11,000 kg",
-        "wingspan": "11 m",
+        "Weight": "11,000 kg",
+        "Wingspan": "11 m",
         "Range": "2,900 km",
         "Top Speed": "2,495 km/h",
         "Engine type": "Eurojet EJ200",
         "Manufacturer(s)": "British Aerospace EAP",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/RAF_Eurofighter_EF-2000_Typhoon_F2_Lofting-1.jpg/300px-RAF_Eurofighter_EF-2000_Typhoon_F2_Lofting-1.jpg",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/RAF_Eurofighter_EF-2000_Typhoon_F2_Lofting-1.jpg/300px-RAF_Eurofighter_EF-2000_Typhoon_F2_Lofting-1.jpg",
         "Countries used": [
             "Austria",
             "Germany",
